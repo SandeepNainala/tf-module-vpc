f67af0146cdee53b069c779d2040f58b684ac3bf
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-08-06 13:06:31 +0530

    Terraform - roboshop - vpc- subnets- azs- igw -eip

--- main.tf
+++ main.tf
@@ -23,3 +23,18 @@ resource "aws_internet_gateway" "igw" {
 
   tags   = merge(var.tags, { Name = "${var.env}-igw"})
 }
+
+resource "aws_eip" "ngw" {
+  count = lookup(lookup(var.subnets, "public", null), "cidr_block", 0)
+  vpc  = true
+  tags = merge(var.tags, { Name = "${var.env}-ngw"})
+}
+
+#resource "aws_nat_gateway" "ngw" {
+#  allocation_id = aws_eip.nat.id
+#  subnet_id     = aws_subnet.public.id
+#
+#  tags = {
+#    Name = "gw NAT"
+#  }
+#}
